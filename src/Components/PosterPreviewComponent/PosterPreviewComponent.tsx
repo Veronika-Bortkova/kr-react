a6435461a7c1f11144ca9cd88463ac83ddf50511
Veronika-Bortkova <shishiga79@gmail.com>
2026-07-21 00:30:51 +0300

    postr placeholder 1

--- src/Components/PosterPreviewComponent/PosterPreviewComponent.tsx
+++ src/Components/PosterPreviewComponent/PosterPreviewComponent.tsx
@@ -1,17 +1,20 @@
 
 import {posterUrl} from "../../constants/urls.ts";
 import type {FC} from "react";
+import defaultImage from "../../imeges/placeholder.jpg";
 type propsType = {
-    poster_path:string
+    poster_path:string | null
     size: string
 }
 
 const PosterPreviewComponent:FC<propsType> = ({poster_path, size}) => {
+    const imageSrc = poster_path ? posterUrl+size+poster_path : defaultImage;
     return (
         <div className={"divPoster"}>
-            <img className={"poster"} src={posterUrl+size+poster_path} alt="poster"></img>
+            <img className={"poster"} src={imageSrc} alt="poster"></img>
         </div>
     );
 };
 
 export default PosterPreviewComponent;
+
