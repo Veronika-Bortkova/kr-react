c09e160eb719ec902104a48462c80b386a5c7c2d
Veronika-Bortkova <shishiga79@gmail.com>
2026-07-23 18:18:18 +0300

    genres bages start

--- src/Components/GenreBadgeComponent/GenreBadgeComponent.tsx
+++ src/Components/GenreBadgeComponent/GenreBadgeComponent.tsx
@@ -1,6 +1,7 @@
 import type {IGenre} from "../../models/GenreModel.ts";
 import type {FC} from "react";
 import "./GenreBadgeComponent.css"
+import {Link} from "react-router-dom";
 
 type PropsType = {
     genre: IGenre;
@@ -9,7 +10,11 @@ type PropsType = {
 const GenreBadgeComponent:FC<PropsType> = ({genre}) => {
     return (
         <>
-            <li><div className={"badge"}>{genre.name}</div></li>
+            <li className={"badgeLi"}>
+                <Link to={"#"} className={"badgeA"}>
+                    <div className={"badge"}>{genre.name}</div>
+                </Link>
+            </li>
 
         </>
     );
